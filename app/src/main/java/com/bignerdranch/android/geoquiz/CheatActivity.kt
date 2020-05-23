774d4f1d0b7425bdac6628484f7c4bba19a2bf07
Colby Sawyer <sawyerc17@students.ecu.edu>
2020-05-23 11:33:54 -0400

    Chapter 6

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+    }
+}
